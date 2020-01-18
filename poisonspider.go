851842c61174df897c8ac39bcfa331166d3049ab
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-01-18 19:32:27 +0900

    Update, file kaminarigumo change poison spider.

--- poisonspider.go
+++ poisonspider.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
-	fmt.Printf("Hello")
+	go func() {
+		//cmd := exec.Command("go", "version")
+		cmd := exec.Command("C:/Ruby26-x64/bin/zinbei", "-d")
+		result, err := cmd.Output()
+		if err != nil {
+			fmt.Println(err)
+			log.Fatalln("This exec is error ...", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", result)
+		os.Exit(0)
+	}()
+
+	for {
+		runtime.Gosched()
+	}
 }
