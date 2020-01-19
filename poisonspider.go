268bab4bdb816361ddbc04eb0f9abb8c456aaa5c
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-01-19 15:05:38 +0900

    Update.

--- poisonspider.go
+++ poisonspider.go
@@ -15,19 +15,33 @@ func main() {
 
 		switch t.Weekday() {
 		case time.Sunday:
+			fmt.Printf("\n")
 			fmt.Println("今日は、日曜日です。")
+			fmt.Printf("\n")
 		case time.Monday:
+			fmt.Printf("\n")
 			fmt.Println("今日は、月曜日です。")
+			fmt.Printf("\n")
 		case time.Tuesday:
+			fmt.Printf("\n")
 			fmt.Println("今日は、火曜日です。")
+			fmt.Printf("\n")
 		case time.Wednesday:
+			fmt.Printf("\n")
 			fmt.Println("今日は、水曜日です。")
+			fmt.Printf("\n")
 		case time.Thursday:
+			fmt.Printf("\n")
 			fmt.Println("今日は、木曜日です。")
+			fmt.Printf("\n")
 		case time.Friday:
+			fmt.Printf("\n")
 			fmt.Println("今日は、金曜日です。")
+			fmt.Printf("\n")
 		case time.Saturday:
+			fmt.Printf("\n")
 			fmt.Println("今日は、土曜日です。")
+			fmt.Printf("\n")
 		}
 
 		//cmd := exec.Command("go", "version")
