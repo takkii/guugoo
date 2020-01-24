1a4cd9796c6077af12dd5c59995186f670344f25
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-01-24 17:23:51 +0900

    Update, HTTP tag get.

---
+++ hypertextransferprotcol.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"runtime"
+)
+
+func main() {
+	go func() {
+		response, err := http.Get("http://standb.herokuapp.com")
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println("status", response.Status)
+
+		body, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(body))
+		os.Exit(0)
+	}()
+
+	for {
+		runtime.Gosched()
+	}
+}
